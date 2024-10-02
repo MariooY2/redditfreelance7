67daecca1e08046d51ab739928780219dfd3a049
Marioo <marioyoussef936@gmail.com>
2024-10-02 15:24:39 +0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}", // If you use the "pages" directory
     "./components/**/*.{js,ts,jsx,tsx}", // If you have a "components" directory
+    "./Components/**/*.{js,ts,jsx,tsx}", // If you have a "components" directory
     "./app/**/*.{js,ts,jsx,tsx}", // If using Next.js app directory (optional)
     "./src/**/*.{js,ts,jsx,tsx}", // If you use src directory
   ],
